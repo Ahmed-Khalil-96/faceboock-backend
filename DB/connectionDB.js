e6e9252805bdadf1c64b12b8ebe3f9333d135981
ahmed-khalil-96 <ahmed.elabassiry96@gmail.com>
2024-06-28 02:04:27 +0300

    upupup

--- DB/connectionDB.js
+++ DB/connectionDB.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
+import mysql2 from 'mysql2';
+
+
 
 export const sequelize = new Sequelize('bq246xnt9nzrt3aewqyi', 'u160sypeem5553zs', 'kBmvZA3U9ErhMCqqQZJT', {
     host: 'bq246xnt9nzrt3aewqyi-mysql.services.clever-cloud.com',
-    dialect:"mysql"
+    dialect:"mysql",
+    dialectModule:mysql2
+
   });
 
 const connection = async()=>{
